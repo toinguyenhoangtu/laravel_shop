43d793b3de89d327ac7c6a4eec10a5232206fa33
Nguyen Hoang Tu <82506580+toinguyenhoangtu@users.noreply.github.com>
2021-11-11 16:42:12 +0700

    prduct relate

--- resources/views/pages/product/show_detail.blade.php
+++ resources/views/pages/product/show_detail.blade.php
@@ -67,113 +67,103 @@
             </div>
         </div>
         <!--/product-details-->
+
+        <div class="category-tab shop-details-tab">
+            <!--category-tab-->
+            <div class="col-sm-12">
+                <ul class="nav nav-tabs">
+                    <li class="active"><a href="#details" data-toggle="tab">Chi tiết</a></li>
+                    <li><a href="#companyprofile" data-toggle="tab">Mô tả</a></li>
+                    <li><a href="#reviews" data-toggle="tab">Đánh Giá</a></li>
+                </ul>
+            </div>
+            <div class="tab-content">
+                <div class="tab-pane fade active in" id="details">
+                    <p>{!! $value->product_content !!}</p>
+                </div>
+
+                <div class="tab-pane fade" id="companyprofile">
+                    <p>{!! $value->product_desc !!}</p>
+                </div>
+
+                <div class="tab-pane fade" id="reviews">
+                    <div class="col-sm-12">
+                        <ul>
+                            <li><a href=""><i class="fa fa-user"></i>EUGEN</a></li>
+                            <li><a href=""><i class="fa fa-clock-o"></i>12:41 PM</a></li>
+                            <li><a href=""><i class="fa fa-calendar-o"></i>31 DEC 2014</a></li>
+                        </ul>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                            labore
+                            et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+                            ut
+                            aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse
+                            cillum dolore eu fugiat nulla pariatur.</p>
+                        <p><b>Write Your Review</b></p>
+
+                        <form action="#">
+                            <span>
+                                <input type="text" placeholder="Your Name" />
+                                <input type="email" placeholder="Email Address" />
+                            </span>
+                            <textarea name=""></textarea>
+                            <b>Rating: </b> <img src="images/product-details/rating.png" alt="" />
+                            <button type="button" class="btn btn-default pull-right">
+                                Submit
+                            </button>
+                        </form>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <!--/category-tab-->
     @endforeach
-    <div class="category-tab shop-details-tab">
-        <!--category-tab-->
-        <div class="col-sm-12">
-            <ul class="nav nav-tabs">
-                <li><a href="#details" data-toggle="tab">Chi tiết</a></li>
-                <li><a href="#companyprofile" data-toggle="tab">Hồ Sơ Công Ty</a></li>
-                <li class="active"><a href="#reviews" data-toggle="tab">Đánh Giá</a></li>
-            </ul>
-        </div>
-        <div class="tab-content">
-            <div class="tab-pane fade" id="details">
-                <div class="col-sm-3">
-                    <div class="product-image-wrapper">
-                        <div class="single-products">
-                            <div class="productinfo text-center">
-                                <img src="images/home/gallery4.jpg" alt="" />
-                                <h2>$56</h2>
-                                <p>Easy Polo Black Edition</p>
-                                <button type="button" class="btn btn-default add-to-cart"><i
-                                        class="fa fa-shopping-cart"></i>Thêm giỏ hàng</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="tab-pane fade" id="companyprofile">
-                <div class="col-sm-3">
-                    <div class="product-image-wrapper">
-                        <div class="single-products">
-                            <div class="productinfo text-center">
-                                <img src="images/home/gallery1.jpg" alt="" />
-                                <h2>$56</h2>
-                                <p>Easy Polo Black Edition</p>
-                                <button type="button" class="btn btn-default add-to-cart"><i
-                                        class="fa fa-shopping-cart"></i>Thêm giỏ hàng</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="tab-pane fade active in" id="reviews">
-                <div class="col-sm-12">
-                    <ul>
-                        <li><a href=""><i class="fa fa-user"></i>EUGEN</a></li>
-                        <li><a href=""><i class="fa fa-clock-o"></i>12:41 PM</a></li>
-                        <li><a href=""><i class="fa fa-calendar-o"></i>31 DEC 2014</a></li>
-                    </ul>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
-                        et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                        aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse
-                        cillum dolore eu fugiat nulla pariatur.</p>
-                    <p><b>Write Your Review</b></p>
-
-                    <form action="#">
-                        <span>
-                            <input type="text" placeholder="Your Name" />
-                            <input type="email" placeholder="Email Address" />
-                        </span>
-                        <textarea name=""></textarea>
-                        <b>Rating: </b> <img src="images/product-details/rating.png" alt="" />
-                        <button type="button" class="btn btn-default pull-right">
-                            Submit
-                        </button>
-                    </form>
-                </div>
-            </div>
-
-        </div>
-    </div>
-    <!--/category-tab-->
     <div class="recommended_items">
         <!--recommended_items-->
-        <h2 class="title text-center">Sản phẩm gợi ý</h2>
+        <h2 class="title text-center">Sản phẩm liên quan</h2>
 
         <div id="recommended-item-carousel" class="carousel slide" data-ride="carousel">
             <div class="carousel-inner">
                 <div class="item active">
-                    <div class="col-sm-4">
-                        <div class="product-image-wrapper">
-                            <div class="single-products">
-                                <div class="productinfo text-center">
-                                    <img src="images/home/recommend1.jpg" alt="" />
-                                    <h2>$56</h2>
-                                    <p>Easy Polo Black Edition</p>
-                                    <button type="button" class="btn btn-default add-to-cart"><i
-                                            class="fa fa-shopping-cart"></i>Thêm giỏ hàng</button>
+                    @foreach ($relate as $key => $values)
+                        <div class="col-sm-4">
+                            <div class="product-image-wrapper">
+                                <div class="single-products">
+                                    <div class="productinfo text-center">
+                                        <img src="{{ URL::to('public/upload/product/' . $values->product_image) }}"
+                                            alt="" />
+                                        <h2>{{ number_format($values->product_price) . ' ' . 'VNĐ' }}</h2>
+                                        <p>{{ $values->product_name }}</p>
+                                        <a href="#" class="btn btn-default add-to-cart"><i
+                                                class="fa fa-shopping-cart"></i>Thêm
+                                            giỏ
+                                            hàng</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    @endforeach
                 </div>
                 <div class="item">
-                    <div class="col-sm-4">
-                        <div class="product-image-wrapper">
-                            <div class="single-products">
-                                <div class="productinfo text-center">
-                                    <img src="images/home/recommend1.jpg" alt="" />
-                                    <h2>$56</h2>
-                                    <p>Easy Polo Black Edition</p>
-                                    <button type="button" class="btn btn-default add-to-cart"><i
-                                            class="fa fa-shopping-cart"></i>Thêm giỏ hàng</button>
+                    @foreach ($relate as $key => $values)
+                        <div class="col-sm-4">
+                            <div class="product-image-wrapper">
+                                <div class="single-products">
+                                    <div class="productinfo text-center">
+                                        <img src="{{ URL::to('public/upload/product/' . $values->product_image) }}"
+                                            alt="" />
+                                        <h2>{{ number_format($values->product_price) . ' ' . 'VNĐ' }}</h2>
+                                        <p>{{ $values->product_name }}</p>
+                                        <a href="#" class="btn btn-default add-to-cart"><i
+                                                class="fa fa-shopping-cart"></i>Thêm
+                                            giỏ
+                                            hàng</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    @endforeach
                 </div>
             </div>
             <a class="left recommended-item-control" href="#recommended-item-carousel" data-slide="prev">
